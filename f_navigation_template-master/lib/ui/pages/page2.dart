39e026e3289719f727381a9749726d1f2dca38fd
jjporto <josearielromeroarias@gmail.com>
2024-04-08 10:07:04 -0500

    Navegación entre pantallas Flutter

---
+++ f_navigation_template-master/lib/ui/pages/page2.dart
@@ -0,0 +1,56 @@
+import 'package:f_navigation_template/ui/pages/page3a.dart';
+import 'package:f_navigation_template/ui/pages/page3b.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Page2 extends StatelessWidget {
+  Page2({super.key});
+  // Obtain the name parameter from the route
+  final String? name = 'some name';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome $name!'),
+        actions: [
+          IconButton(
+              //todo: Implement back navigation logic making sure to clean the navigation stack
+              onPressed: () => null,
+              icon: const Icon(Icons.logout))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have successfully navigated to this page.',
+            ),
+            const SizedBox(height: 20),
+            const Text('Do you want option A or B?'),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  //todo: Implement navigation to page3a sending the name as a parameter
+                  onPressed: () => Get.to(Page3A()),
+                  child: const Text(
+                    'Option A',
+                  ),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                    onPressed: () => Get.to(Page3B()),
+                    child: const Text(
+                      'Option B',
+                    )),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
